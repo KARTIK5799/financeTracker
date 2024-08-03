e957a1e8d451d3bd593f3c7eebd1ea5e11c7a170
KARTIK5799 <thekartiksapkal@gmail.com>
2024-08-03 12:01:02 +0530

    basic data integration is done

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignUpModel } from '../loginsignup/loginsignup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accounts.component.html',
-  styleUrl: './accounts.component.css'
+  styleUrls: ['./accounts.component.css']
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit {
+  storedData: SignUpModel[] = [];
 
+  ngOnInit() {
+    const data = localStorage.getItem('angular17users');
+
+    if (data) {
+      try {
+        this.storedData = JSON.parse(data);
+      } catch (e) {
+        console.error('Error parsing local storage data', e);
+      }
+    } else {
+      console.log('No data found in local storage');
+    }
+  }
+
+  getAccounts() {
+    return this.storedData.flatMap(user => user.accounts);
+  }
 }
--- src/app/components/dashboard-content/dashboard-content.component.ts
+++ src/app/components/dashboard-content/dashboard-content.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignUpModel } from '../loginsignup/loginsignup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-content.component.html',
-  styleUrl: './dashboard-content.component.css'
+  styleUrls: ['./dashboard-content.component.css']
 })
-export class DashboardContentComponent {
+export class DashboardContentComponent implements OnInit {
+  
+  storedData: SignUpModel[] = [];
+  totalBalance: number = 0;
+  totalIncome: number = 0;
+  totalSpendings: number = 0;
+  transactionHistory: any[] = [];
 
+  ngOnInit() {
+    const data = localStorage.getItem('angular17users');
+
+    if (data) {
+      try {
+        this.storedData = JSON.parse(data);
+        this.calculateTotals();
+        this.prepareTransactionHistory();
+      } catch (e) {
+        console.error('Error parsing local storage data', e);
+      }
+    } else {
+      console.log('No data found in local storage');
+    }
+  }
+
+  calculateTotals() {
+    let balance = 0;
+    let income = 0;
+    let spendings = 0;
+
+    this.storedData.forEach(user => {
+      user.accounts.forEach(account => {
+        balance += account.balance;
+        account.transactions.forEach(transaction => {
+          if (transaction.type === 'income') {
+            income += transaction.amount;
+          } else {
+            spendings += transaction.amount;
+          }
+        });
+      });
+    });
+
+    this.totalBalance = balance;
+    this.totalIncome = income;
+    this.totalSpendings = spendings;
+  }
+
+  prepareTransactionHistory() {
+    this.transactionHistory = this.storedData.flatMap(user =>
+      user.accounts.flatMap(account => account.transactions)
+    ).map(transaction => ({
+      ...transaction,
+      date: new Date(transaction.date) 
+    }))
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+  }
 }
